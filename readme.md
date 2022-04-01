60c6b50939b7f4c9ca697c39da206b69ad511842
Hennadii Shpak <borchuga@gmail.com>
2022-04-01 07:58:11 +0300

    minor fix

---
+++ readme.md
@@ -0,0 +1,6 @@
+### Команды:
+
+- `npm start` &mdash; start server in production mode
+- `npm run start:dev` &mdash; start server in development mode
+- `npm run lint` &mdash; start code check with eslint
+- `npm lint:fix` &mdash; the same linter check, but with automatic correction of simple errors
--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const { DB_HOST, PORT = 4000 } = process.env;
+const { DB_HOST, PORT = 3001 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() =>
     app.listen(PORT, () => {
-      console.log('Database tru and we listen port 4000');
+      console.log('Database tru and we listen port 3001');
     }),
   )
   .catch(error => {
-    console.log('Ой-йо-йой', error.message);
+    console.log('Error', error.message);
     process.exit(1);
   });
