abff0980219bc21adc55403e9ac1e837b4d5f106
Oleksandr Balitskiy <popingalov@gmail.com>
2022-04-03 23:39:30 +0300

    Update getAllTransactions.js

--- controllers/transactions/getAllTransactions.js
+++ controllers/transactions/getAllTransactions.js
@@ -1,4 +1,4 @@
-const { Transaction } = require("../../models");
+const { Transaction } = require('../../models');
 
 const getAllTransactions = async (req, res, next) => {
   const { page = 1, limit = 6 } = req.query;
@@ -7,14 +7,13 @@ const getAllTransactions = async (req, res, next) => {
 
   const transactions = await Transaction.find(
     { owner: _id },
-    "-createdAt -updatedAt",
+    '-createdAt -updatedAt',
     {
       skip,
       limit: Number(limit),
-    }
-  ).populate("owner", "email");
-
-  res.json(transactions);
+    },
+  ).populate('owner', 'email');
+  !transactions ? res.json([]) : res.json(transactions);
 };
 
 module.exports = getAllTransactions;
