33a9dd2210302e03bfb0fe9dd1ab02b15a230764
VitaliiPrykhodko <vitaliimanager1@gmail.com>
2022-04-11 21:54:03 +0300

    swagger well done

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('./sawgger.json')
-
+const swaggerDoc = require('./swagger.json');
 
 const {
   authRouter,
   transactionsRouter,
   statisticsRouter,
   categoriesRouter,
-} = require("./routes");
-const { notFound, serverError } = require("./libs/http-responses");
+} = require('./routes');
+const { notFound, serverError } = require('./libs/http-responses');
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/api/users", authRouter);
-app.use("/api/transactions", transactionsRouter);
-app.use("/api/statistics", statisticsRouter);
-app.use("/api/categories", categoriesRouter);
+app.use('/api/users', authRouter);
+app.use('/api/transactions', transactionsRouter);
+app.use('/api/statistics', statisticsRouter);
+app.use('/api/categories', categoriesRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use((req, res) => {
--- controllers/categories/remove.js
+++ controllers/categories/remove.js
@@ -1,44 +1,40 @@
 const { Category } = require('../../models');
 const CreateError = require('http-errors');
 
-const {
-    ok,
-    notFound
-} = require('../../libs').HTTP_RESPONSES;
-
+const { ok, notFound } = require('../../libs').HTTP_RESPONSES;
 
 const removeCategory = async (req, res, next) => {
-    const { categoryId } = req.params;
-    const { collectionId, isEnglishVersion } = req.body;
-    let deletedCategory;
-    
-    const categoryList = await Category.findById(collectionId);
+  const { categoryId } = req.params;
+  const { collectionId, isEnglishVersion } = req.body;
+  let deletedCategory;
 
-    if(isEnglishVersion) {
-        Object.entries(categoryList.en).forEach((item, i ) => {
-            if(item[1].id === categoryId) {
-                deletedCategory = {...categoryList.en[i]};
+  const categoryList = await Category.findById(collectionId);
 
-                categoryList.en.splice(i, 1)
-            }
-        })
-    } else {
-        Object.entries(categoryList.ru).forEach((item, i )=> {
-            if(item[1].id === categoryId) {
-                deletedCategory = {...categoryList.ru[i]};
+  if (isEnglishVersion) {
+    Object.entries(categoryList.en).forEach((item, i) => {
+      if (item[1].id === categoryId) {
+        deletedCategory = { ...categoryList.en[i] };
 
-                categoryList.ru.splice(i, 1)
-            }
-        })
-    }
+        categoryList.en.splice(i, 1);
+      }
+    });
+  } else {
+    Object.entries(categoryList.ru).forEach((item, i) => {
+      if (item[1].id === categoryId) {
+        deletedCategory = { ...categoryList.ru[i] };
 
-    await Category.findByIdAndUpdate(collectionId, categoryList)
+        categoryList.ru.splice(i, 1);
+      }
+    });
+  }
 
-    if(deletedCategory) {
-        res.status(ok.code).json(categoryList)
-    } else {
-        throw new CreateError(notFound.code, notFound.status)
-    }
-}
+  await Category.findByIdAndUpdate(collectionId, categoryList);
+
+  if (deletedCategory) {
+    res.status(ok.code).json(categoryList);
+  } else {
+    throw new CreateError(notFound.code, notFound.status);
+  }
+};
 
 module.exports = removeCategory;
