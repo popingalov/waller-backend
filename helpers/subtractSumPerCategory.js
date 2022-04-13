050f1706ee3ed654198b9e9ca06001cf68909231
Oleksandr Balitskiy <popingalov@gmail.com>
2022-04-13 20:32:10 +0300

    Update subtractSumPerCategory.js

--- helpers/subtractSumPerCategory.js
+++ helpers/subtractSumPerCategory.js
@@ -4,7 +4,7 @@ function subtractSumPerCategory(transactions, color) {
 
     for (const stat of stats) {
       if (stat.category === transaction.category) {
-        stat.type === '+'
+        stat.type === '-'
           ? (stat.sum += transaction.amount)
           : (stat.sum -= transaction.amount);
         return stats;
