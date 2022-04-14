faff70cf23ed88e67f8380f39f6eb9a10aae1b56
Hennadii_Shpak <borchuga@gmail.com>
2022-04-15 00:10:19 +0300

    added date field in new transaction

--- controllers/transactions/createTransaction.js
+++ controllers/transactions/createTransaction.js
@@ -1,11 +1,23 @@
+const CreateError = require('http-errors');
+
 const { created } = require('../../libs/http-responses');
 const { Transaction } = require('../../models');
 const { calculateCurrentBalance, longOperation } = require('../../helpers');
 const { getTransactions } = require('../../helpers');
+const { dataError } = require('../../libs').HTTP_RESPONSES;
 
 const createTransaction = async (req, res, next) => {
   const { _id } = req.user;
-  const { amount, type, dataFiltr: filter, triger = null } = req.body;
+  const { date, amount, type } = req.body;
+
+  const normalizedDate = date.split('.').reverse().join('.');
+  const transactionTimeStamp = new Date(normalizedDate).getTime();
+
+  const dateNow = new Date().getTime();
+
+  if (transactionTimeStamp > dateNow) {
+    throw new CreateError(dataError.code, dataError.status);
+  }
 
   const transactions = await Transaction.find(
     { owner: _id },
@@ -15,17 +27,15 @@ const createTransaction = async (req, res, next) => {
   //   const dateTime = new Date().getTime();
   //   const dateNow = new Date(dateTime).toLocaleDateString();
   let currentBalance = 0;
-  if (triger) {
+  if (transactionTimeStamp < dateNow) {
     currentBalance = await longOperation({
       transactions,
       type,
       amount,
-      filter,
+      date,
       _id,
     });
-  }
-
-  if (!triger) {
+  } else {
     currentBalance = await calculateCurrentBalance({
       transactions,
       type,
