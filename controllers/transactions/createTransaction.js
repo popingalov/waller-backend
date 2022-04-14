b97b3855fb4eaa34c0544879502d4df1cfcfc844
Oleksandr Balitskiy <popingalov@gmail.com>
2022-04-15 02:11:17 +0300

    dd

--- controllers/transactions/createTransaction.js
+++ controllers/transactions/createTransaction.js
@@ -10,30 +10,41 @@ const createTransaction = async (req, res, next) => {
   const { _id } = req.user;
   const { date, amount, type } = req.body;
 
-  const msPerDay = 86399999;
+  //   const msPerDay = 86399999;
+
+  //   const filter = new Date(normalizedDate).getTime();
+  //   const comparedTimeStamp = filter + msPerDay;
+  //   console.log(filter);
 
   const normalizedDate = date.split('.').reverse().join('.');
-  const filter = new Date(normalizedDate).getTime();
-  const comparedTimeStamp  = filter + msPerDay;
+  //
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+  const currentDate = dd + '.' + mm + '.' + yyyy;
+  const normilizedCurrentDate = currentDate.split('.').reverse().join('.');
+  //
+  const timForLongOperation = new Date(normalizedDate).getTime();
+  //
+  console.log(new Date(normalizedDate).getTime() < today.getTime());
 
-  const dateNow = new Date().getTime();
-
-  if (comparedTimeStamp > dateNow) {
+  if (normalizedDate > normilizedCurrentDate) {
     throw new CreateError(dataError.code, dataError.status);
   }
 
   const transactions = await Transaction.find(
     { owner: _id },
     '-createdAt -updatedAt',
-  ).sort({ dataFiltr: -1 });
-  
+  ).sort({ dataFiltr: +1 });
+
   let currentBalance = 0;
-  if (comparedTimeStamp < dateNow) {
+  if (normalizedDate < normilizedCurrentDate) {
     currentBalance = await longOperation({
       transactions,
       type,
       amount,
-      filter,
+      filter: timForLongOperation,
       _id,
     });
   } else {
@@ -46,7 +57,7 @@ const createTransaction = async (req, res, next) => {
 
   const newTransaction = {
     ...req.body,
-    dataFiltr: dateNow,
+    dataFiltr: today.getTime(),
     balance: currentBalance || amount,
     owner: req.user._id,
   };
@@ -54,6 +65,7 @@ const createTransaction = async (req, res, next) => {
   await Transaction.create(newTransaction);
   const transactionsList = await getTransactions(_id);
   res.status(created.code).json(transactionsList);
+  res.status(created.code).json('transactionsList');
 };
 
 module.exports = createTransaction;
