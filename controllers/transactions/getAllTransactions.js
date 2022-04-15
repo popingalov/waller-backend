7fa1ef7f6ebbe7e7b4271ca7e3d1b4e99cb93160
Oleksandr Balitskiy <popingalov@gmail.com>
2022-04-15 16:42:54 +0300

    fix

--- controllers/transactions/getAllTransactions.js
+++ controllers/transactions/getAllTransactions.js
@@ -1,11 +1,16 @@
 const { getTransactions, countPagesQuantity } = require('../../helpers');
 const { ok } = require('../../libs/http-responses');
-
+const { Transaction } = require('../../models');
 const getAllTransactions = async (req, res, next) => {
   const { page = 1, limit = 6 } = req.query;
   const { _id } = req.user;
 
-  const allTransactions = await getTransactions(_id);
+  const allTransactions = await Transaction.find(
+    { owner: _id },
+    '-createdAt -updatedAt',
+  )
+    .sort({ dataFiltr: -1 })
+    .populate('owner', 'email');
   const transactions = await getTransactions(_id, page, limit);
 
   const currentBalance = allTransactions[0].balance;
