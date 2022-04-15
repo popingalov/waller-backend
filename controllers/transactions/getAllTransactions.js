fc935ffbeb74a817ac9e10ba7686012b9a0fa419
Oleksandr Balitskiy <popingalov@gmail.com>
2022-04-15 16:29:44 +0300

    asd

--- controllers/transactions/getAllTransactions.js
+++ controllers/transactions/getAllTransactions.js
@@ -1,24 +1,25 @@
 const { getTransactions, countPagesQuantity } = require('../../helpers');
-const { Transaction } = require('../../models');
 const { ok } = require('../../libs/http-responses');
 
 const getAllTransactions = async (req, res, next) => {
   const { page = 1, limit = 6 } = req.query;
   const { _id } = req.user;
 
-  const allTransactions = await Transaction.find({ owner: _id }, '-createdAt -updatedAt');
+  const allTransactions = await getTransactions(_id);
   const transactions = await getTransactions(_id, page, limit);
 
-  const currentBalance = allTransactions.at(-1).balance;
-  const pages = countPagesQuantity({allTransactions, limit})  
+  const currentBalance = allTransactions[0].balance;
+  const pages = countPagesQuantity({ allTransactions, limit });
 
   const responseObj = {
     transactions,
     currentBalance,
-    pages
-  }
-  
-  !transactions ? res.status(ok.code).json([]) : res.status(ok.code).json(responseObj);
+    pages,
+  };
+
+  !transactions
+    ? res.status(ok.code).json([])
+    : res.status(ok.code).json(responseObj);
 };
 
 module.exports = getAllTransactions;
